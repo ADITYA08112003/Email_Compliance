34bcb0c9f9b7bf7b8fa0e30e5af64dfe0d8d4284
Aditya Raj <105595814+ADITYA08112003@users.noreply.github.com>
2026-07-17 06:54:45 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+﻿from flask import Flask, render_template, request
+from ai.preprocess import clean_email
+from ai.classifier import classify_email
+from ai.risk_engine import get_risk_score, get_priority
+from config import CATEGORY_CLASSIFICATION
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    result = None
+
+    if request.method == "POST":
+        email = request.form["email"]
+
+        cleaned = clean_email(email)
+
+        category, reason = classify_email(cleaned)
+
+        risk_score = get_risk_score(category)
+
+        priority = get_priority(risk_score)
+
+        classification = CATEGORY_CLASSIFICATION.get(
+            category,
+            "True Positive" if risk_score >= 60 else "False Positive"
+        )
+
+        result = {
+            "email": email,
+            "category": category,
+            "reason": reason,
+            "risk_score": risk_score,
+            "priority": priority,
+            "classification": classification
+        }
+
+    return render_template("index.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,19 @@
+PRIORITY_MATRIX = {
+    "Secrecy": 90,
+    "Market Manipulation / Misconduct": 95,
+    "Market Bribery": 98,
+    "Change in communication": 60,
+    "Complaints": 50,
+    "Disclaimer": 70,
+    "Employee ethics": 85
+}
+
+CATEGORY_CLASSIFICATION = {
+    "Secrecy": "False Positive",
+    "Market Manipulation / Misconduct": "True Positive",
+    "Market Bribery": "True Positive",
+    "Change in communication": "True Positive",
+    "Complaints": "False Positive",
+    "Disclaimer": "True Positive",
+    "Employee ethics": "True Positive"
+}
